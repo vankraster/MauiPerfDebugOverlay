d311dd69e9fff56dfbfb72b3ea1a216811c7451f
Radu Bogdan Claudiu <bogdan_radu@beenear.com>
2025-08-26 15:13:43 +0300

    optimized battery for android

--- MauiPerfDebugOverlay/Platforms/Android/BatteryService.cs
+++ MauiPerfDebugOverlay/Platforms/Android/BatteryService.cs
@@ -2,33 +2,36 @@
 using Android.Content;
 using Android.OS;
 using Application = Android.App.Application;
+
+
+
+
 namespace MauiPerfDebugOverlay.Platforms
 {
     public static class BatteryService
     {
+        private static readonly BatteryManager _batteryManager;
+        private static readonly IntentFilter _batteryFilter; 
+
+        static BatteryService()
+        { 
+            _batteryManager = Application.Context.GetSystemService(Context.BatteryService) as BatteryManager;
+            _batteryFilter = new IntentFilter(Intent.ActionBatteryChanged);
+        }
 
         public static double GetBatteryMilliW()
         {
-            double _batteryMilliW = 0;
-            var context = Application.Context;
-            var bm = (BatteryManager)context.GetSystemService(Context.BatteryService);
-            var filter = new IntentFilter(Intent.ActionBatteryChanged);
-            var batteryStatus = context.RegisterReceiver(null, filter);
+            var batteryStatus = Application.Context.RegisterReceiver(null, _batteryFilter);
 
             int voltage = batteryStatus?.GetIntExtra(BatteryManager.ExtraVoltage, -1) ?? -1; // mV
-            int currentMicroA = bm?.GetIntProperty((int)BatteryProperty.CurrentNow) ?? -1; // µA
+            int currentMicroA = _batteryManager?.GetIntProperty((int)BatteryProperty.CurrentNow) ?? -1; // µA
 
             if (voltage > 0 && currentMicroA > 0)
             {
-                // Calcul în mW: (mV * mA)
-                _batteryMilliW = (currentMicroA / 1000.0) * (voltage / 1000.0);
-            }
-            else
-            {
-                _batteryMilliW = 0;
+                return (currentMicroA / 1000.0) * (voltage / 1000.0); // mW
             }
 
-            return _batteryMilliW;
+            return 0;
         }
     }
 }
